5eefe04da99ecd112f69a3747e0ea248e0c18984
Jackson <dmitrievei92@gmail.com>
2021-03-28 23:01:20 +0300

    update

---
+++ File Manager/File Manager/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace File_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backButton = new System.Windows.Forms.Button();
+            this.goButton = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Label = new System.Windows.Forms.Label();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fileTypeLabel = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(13, 13);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(713, 13);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(94, 15);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(612, 20);
+            this.filePathTextBox.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(13, 43);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(775, 374);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(13, 424);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(54, 13);
+            this.Label.TabIndex = 4;
+            this.Label.Text = "File Name";
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(77, 424);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(13, 13);
+            this.fileNameLabel.TabIndex = 4;
+            this.fileNameLabel.Text = "--";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(671, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "File Type";
+            // 
+            // fileTypeLabel
+            // 
+            this.fileTypeLabel.AutoSize = true;
+            this.fileTypeLabel.Location = new System.Drawing.Point(726, 428);
+            this.fileTypeLabel.Name = "fileTypeLabel";
+            this.fileTypeLabel.Size = new System.Drawing.Size(13, 13);
+            this.fileTypeLabel.TabIndex = 4;
+            this.fileTypeLabel.Text = "--";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "avi_file_25831.png");
+            this.imageList1.Images.SetKeyName(1, "bmp_file_25830.png");
+            this.imageList1.Images.SetKeyName(2, "cab_file_25829.png");
+            this.imageList1.Images.SetKeyName(3, "desktop_25827.png");
+            this.imageList1.Images.SetKeyName(4, "doc_file_25826.png");
+            this.imageList1.Images.SetKeyName(5, "folder_back_25859.png");
+            this.imageList1.Images.SetKeyName(6, "folder_close_25858.png");
+            this.imageList1.Images.SetKeyName(7, "folder_open_25856.png");
+            this.imageList1.Images.SetKeyName(8, "gif_file_25825.png");
+            this.imageList1.Images.SetKeyName(9, "jpg_file_25824.png");
+            this.imageList1.Images.SetKeyName(10, "mov_file_25823.png");
+            this.imageList1.Images.SetKeyName(11, "mp3_file_25822.png");
+            this.imageList1.Images.SetKeyName(12, "mp4_file_25821.png");
+            this.imageList1.Images.SetKeyName(13, "mpeg_file_25820.png");
+            this.imageList1.Images.SetKeyName(14, "pdf_file_25819.png");
+            this.imageList1.Images.SetKeyName(15, "png_file_25818.png");
+            this.imageList1.Images.SetKeyName(16, "psd_files_25817.png");
+            this.imageList1.Images.SetKeyName(17, "rar_file_25816.png");
+            this.imageList1.Images.SetKeyName(18, "tif_file_25815.png");
+            this.imageList1.Images.SetKeyName(19, "txt_file_25814.png");
+            this.imageList1.Images.SetKeyName(20, "wma_file_25813.png");
+            this.imageList1.Images.SetKeyName(21, "zip_file_25812.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fileTypeLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.backButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label fileTypeLabel;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ File Manager/File Manager/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Manager
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = @"C:\Users\Jackson\Downloads";
+        private string currentlySelectedItemName = "";
+        private bool isFile = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filePathTextBox.Text = filePath;
+            loadFilesAndDirectories();
+        }
+
+        /// <summary>
+        /// Заполняет ListView файлами и директориями
+        /// </summary>
+        public void loadFilesAndDirectories()
+        {
+            DirectoryInfo fileList;
+            string tempFilePath = "";
+            FileAttributes fileAttr;
+            try
+            {
+
+                if (isFile)
+                {
+                    tempFilePath = filePath + @"\" + currentlySelectedItemName;
+                    FileInfo fileDetails = new FileInfo(tempFilePath);
+                    fileNameLabel.Text = fileDetails.Name;
+                    fileTypeLabel.Text = fileDetails.Extension;
+                    fileAttr = File.GetAttributes(tempFilePath);
+                    Process.Start(tempFilePath);
+                }
+                else
+                {
+
+                    fileAttr = File.GetAttributes(filePath);
+
+                }
+
+                if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    fileList = new DirectoryInfo(filePath);
+                    FileInfo[] files = fileList.GetFiles();           //Возвращает все файлы
+                    DirectoryInfo[] dirs = fileList.GetDirectories(); //Возвращает все директории
+
+                    string fileExtension = "";
+                    listView1.Items.Clear();
+
+                    for (int i = 0; i < dirs.Length; i++)
+                    {
+                        listView1.Items.Add(dirs[i].Name, 21);
+                    }
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+
+
+                        fileExtension = files[i].Extension.ToUpper();
+                        switch (fileExtension)
+                        {
+                            case ".MP3":
+                            case ".MP2":
+                                listView1.Items.Add(dirs[i].Name, 9);
+                                break;
+                            case ".MKV":
+                            case ".AVI":
+                            case ".MPEG":
+                            case ".MOV":
+                            case ".WMA":
+                                listView1.Items.Add(dirs[i].Name, 10);
+                                break;
+                            case ".PDF":
+                                listView1.Items.Add(dirs[i].Name, 12);
+                                break;
+                            case ".DOC":
+                            case ".DOCX":
+                            case ".TXT":
+                                listView1.Items.Add(dirs[i].Name, 3);
+                                break;
+                            case ".BMP":
+                            case ".GIF":
+                                listView1.Items.Add(dirs[i].Name, 6);
+                                break;
+                            case ".PNG":
+                                listView1.Items.Add(dirs[i].Name, 13);
+                                break;
+
+                            case ".PSD":
+                                listView1.Items.Add(dirs[i].Name, 14);
+                                break;
+                            case ".RAR":
+                            case ".ZIP":
+                                listView1.Items.Add(dirs[i].Name, 15);
+                                break;
+
+                            default:
+                                listView1.Items.Add(dirs[i].Name, 20);
+                                break;
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    fileNameLabel.Text = currentlySelectedItemName;
+                }
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+
+        public void loadButtonAction()
+        {
+            removeBackSlash();
+            filePath = filePathTextBox.Text;
+            loadFilesAndDirectories();
+            isFile = false;
+        }
+
+        public void removeBackSlash()
+        {
+            string path = filePathTextBox.Text;
+            if (path.LastIndexOf(@"\") == path.Length - 1)
+            {
+                filePathTextBox.Text = path.Substring(0, path.Length - 1); ;
+            }
+        }
+
+        public void goBack()
+        {
+            try
+            {
+                removeBackSlash();
+                string path = filePathTextBox.Text;
+                path = path.Substring(0, path.LastIndexOf(@"\"));
+                isFile = false;
+                filePathTextBox.Text = path;
+                removeBackSlash();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            loadButtonAction();
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            currentlySelectedItemName = e.Item.Text;
+
+            FileAttributes fileAttr = File.GetAttributes(filePath + @"\" + currentlySelectedItemName);
+            if ((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                isFile = false;
+                filePathTextBox.Text = filePath + @"\" + currentlySelectedItemName;
+            }
+            else
+            {
+                isFile = true;
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            loadButtonAction();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            goBack();
+            loadButtonAction();
+        }
+    }
+}
